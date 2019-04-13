f7d6b8e52cd45aaace235e6f5e40381924ad800b
Lucas Nogueira <lucasmoises93@gmail.com>
2019-04-13 19:41:21 -0300

    Null, Undefined e suas limitaçoes

--- nullEUndefined.js
+++ nullEUndefined.js
@@ -12,3 +12,25 @@ let d = c // Cópia por valor, pois estamos usando tipos primitivos da linguagem
 d++
 console.log(d)
 console.log(c)
+
+let valor // não inicializada
+console.log(valor)
+valor = null // Null, ausência de valor. Não aponta pra nenhum endereço de memória
+console.log(valor)
+
+// Sempre configurar como Null quando quiser zerar o valor de uma variável.
+
+// console.log(valor.toString())
+
+const produto = {}
+console.log(produto.preco)
+produto.preco = 3.50
+console.log(produto)
+
+produto.preco = undefined // Evite atribuir undefined
+console.log(!!produto.preco)
+// delete produto.preco para deletar atributo do objeto
+console.log(produto)
+produto.preco = null // Sem preço
+console.log(!!produto.preco)
+console.log(produto)
