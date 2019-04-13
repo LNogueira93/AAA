2ab6b9d202136b9b60943a0f603e15e4da9f8560
Lucas Nogueira <lucasmoises93@gmail.com>
2019-04-13 19:11:12 -0300

    Objetos

--- Fundamentos/objeto.js
+++ Fundamentos/objeto.js
@@ -1,6 +1,9 @@
 // JSON (JavaScript Object Notation)
+// Objeto é uma coleção de chaves e valores
 
 const prod1 = {}
 prod1.nome = 'Celular Ultra Mega'
+prod1.preco = 4998.90
+prod1['Desconto Legal'] = 0.40 //Evitar atributos com espaço
 
-// Objeto é uma coleção de chaves e valores
+console.log(prod1)
