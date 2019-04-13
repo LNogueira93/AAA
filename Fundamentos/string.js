c647c784694789beb5d25135887456ddc89695bf
Lucas Nogueira <lucasmoises93@gmail.com>
2019-04-13 18:08:01 -0300

    Manipulando strins

--- Fundamentos/string.js
+++ Fundamentos/string.js
@@ -3,3 +3,6 @@ const escola = 'Cod3r'
 console.log(escola.charAt(4))
 console.log(escola.charAt(2))
 console.log(escola.charCodeAt(3)) // Retorna a posição do item na tabela Unicode.
+console.log(escola.indexOf('3'))
+console.log(escola.substring(1))
+console.log(escola.substring(0, 3))
