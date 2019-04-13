58a3f1cd71edeba4beb9442cc9172f9073546e68
Lucas Nogueira <lucasmoises93@gmail.com>
2019-04-13 17:28:06 -0300

    Inicio fundamentos JS

---
+++ Fundamentos/variaveisEConstantes.js
@@ -0,0 +1,19 @@
+var a = 3
+console.log(a)
+let b = 1
+console.log(b)
+var c = a + b
+console.log(c)
+d = 'Lucas'
+console.log(d)
+e = 3.14
+console.log(e)
+f = false
+console.group(f)
+
+console.log(typeof(a))
+console.log(typeof(b))
+console.log(typeof(c))
+console.log(typeof(d))
+console.log(typeof(e))
+console.log(typeof(f))
