abe07e30ff074ea6e196dcc4631db8d2f9971f64
Lucas Nogueira <lucasmoises93@gmail.com>
2019-04-13 18:52:20 -0300

    Booleanos

--- Fundamentos/booleanos.js
+++ Fundamentos/booleanos.js
@@ -7,3 +7,27 @@ console.log(isAtivo)
 isAtivo = 1
 console.log(!!isAtivo) // Negação da negação = True
 console.log(!isAtivo) // Negação = False
+
+console.log('Os verdadeiros...')
+console.log(!!3)
+console.log(!!-1)
+console.log(!!' ')
+console.log(!!'Texto')
+console.log(!![])
+console.log(!!{})
+console.log(!!Infinity)
+console.log(!!isAtivo == true)
+
+console.log('Os falsos...')
+console.log(!!0)
+console.log(!!'')
+console.log(!!null)
+console.log(!!NaN)
+console.log(!!undefined)
+console.log(!!(isAtivo = false))
+
+console.log('Pra finalizar...')
+console.log(!!('' || null || 0 || ' '))
+
+let nome = 'Lucas'
+console.log(nome || 'Desconhecido') // Devolve 'Desconhecido' caso a variavel não receba valor algum.
