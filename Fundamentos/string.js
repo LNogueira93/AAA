e2e9a7617eae6937cb868fcf785f46d18ed2fe95
Lucas Nogueira <lucasmoises93@gmail.com>
2019-04-13 18:11:11 -0300

    Manipulando strings

--- Fundamentos/string.js
+++ Fundamentos/string.js
@@ -3,6 +3,6 @@ const escola = 'Cod3r'
 console.log(escola.charAt(4))
 console.log(escola.charAt(2))
 console.log(escola.charCodeAt(3)) // Retorna a posição do item na tabela Unicode.
-console.log(escola.indexOf('3'))
-console.log(escola.substring(1))
-console.log(escola.substring(0, 3))
+console.log(escola.indexOf('3')) // Retorna o item na posiçao 3.
+console.log(escola.substring(1)) // Vai do índice 1 até o fim da string
+console.log(escola.substring(0, 3)) // Vai do índice 0 ao 2, não inclui o indice 3
