5fd908c66b9fbbeb8eddc0e67c72f9c693f8bf0b
Lucas Nogueira <lucasmoises93@gmail.com>
2019-04-13 18:14:43 -0300

    Manipulando strings

--- Fundamentos/string.js
+++ Fundamentos/string.js
@@ -6,3 +6,6 @@ console.log(escola.charCodeAt(3)) // Retorna a posição do item na tabela Unico
 console.log(escola.indexOf('3')) // Retorna o item na posiçao 3.
 console.log(escola.substring(1)) // Vai do índice 1 até o fim da string
 console.log(escola.substring(0, 3)) // Vai do índice 0 ao 2, não inclui o indice 3
+
+console.log('Escola '.concat(escola).concat('!'))
+console.log(escola.replace(3, 'e'))
