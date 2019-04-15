4599d38955ff943107c5ab603cafd91687675fda
Lucas Nogueira <lucasmoises93@gmail.com>
2019-04-14 22:42:43 -0300

    Funções parte 2

---
+++ Fundamentos/funcao2.js
@@ -0,0 +1,16 @@
+// Armazenando uma função em uma variável
+const imprimirSoma = function (a, b) {
+    console.log(a + b)
+}
+
+imprimirSoma(2, 3)
+
+//Armazenando uma função arrow em uma variável
+
+const soma = (a, b) => {
+    return a + b
+}
+
+console.log(soma(2, 10))
+
+const subtracao = (a, b) => a - b
